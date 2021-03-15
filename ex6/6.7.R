b5cd51daf97e2c839365bbf5f64ca5b4a8a07c83
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-03-15 15:44:31 +0100

    Added sceletons for ex 6.6-6.9

---
+++ ex6/6.7.R
@@ -0,0 +1,12 @@
+# 6-7 Matrices in R -------------------------------------------------------
+
+# >(a) --------------------------------------------------------------------
+# (a) Create a 2 x 2 matrix A by row binding vectors using the rbind() command.
+
+# >(b) --------------------------------------------------------------------
+# (b) Nullify matrix A by adding another matrix that you define.
+
+# >(c) --------------------------------------------------------------------
+# (c) Double all the values in the original matrix
+# A by multiplication with another matrix that you define.
+
---
+++ ex6/6.9.R
@@ -0,0 +1,23 @@
+# 6-9 Clustering and Classification on the Iris Data ----------------------
+
+# >(a) --------------------------------------------------------------------
+# Load the Iris dataset in R, remove the class attribute.
+# Cluster it using k-means with a reasonable choice of k.
+
+# >(b) --------------------------------------------------------------------
+# Use the clustering result to label the data.
+
+# >(c) --------------------------------------------------------------------
+# Create some artificial flower data, that could potentially be Iris flowers.
+# Think about how you will do this,
+# and what you expect the resulting flowers to be.
+
+# >(d) --------------------------------------------------------------------
+# Try the knn-classifier with different values for k,
+# and use your generated labeled Iris dataset to classify the artificial query points.
+
+# >(e) --------------------------------------------------------------------
+# Try using the original labeled Iris dataset. Does this yield the same result?
+
+# >(f) --------------------------------------------------------------------
+# Explain your findings.
