014bb6d2b5cbe3c717f63d8e746803f047949d5c
ADernild <alex@dernild.dk>
2021-03-15 16:59:17 +0100

    Løsninger update

--- ex6/Alexander_solutions.R
+++ ex6/Alexander_solutions.R
@@ -103,8 +103,39 @@ cluster <- kmeans(iris_no_classes, 3)
 
 iris_no_classes['Cluster'] <- cluster$cluster
 
-artificial_data <- data.frame(Sepal.Length = rnorm(mean = mean(iris$Sepal.Length), sd = sd(iris$Sepal.Length), 150),
-                              Sepal.width = rnorm(mean = mean(iris$Sepal.Width), sd = sd(iris$Sepal.Width), 150),
-                              Petal.width = rnorm(mean = mean(iris$Petal.Width), sd = sd(iris$Petal.Width), 150),
-                              Petal.length = rnorm(mean = mean(iris$Petal.Length), sd = sd(iris$Petal.Length), 150))
 
+# > c ---------------------------------------------------------------------
+
+artificial_data <- data.frame(Sepal.Length = abs(rnorm(mean = mean(iris$Sepal.Length), sd = sd(iris$Sepal.Length), 150)),
+                              Sepal.width = abs(rnorm(mean = mean(iris$Sepal.Width), sd = sd(iris$Sepal.Width), 150)),
+                              Petal.width = abs(rnorm(mean = mean(iris$Petal.Width), sd = sd(iris$Petal.Width), 150)),
+                              Petal.length = abs(rnorm(mean = mean(iris$Petal.Length), sd = sd(iris$Petal.Length), 150)),
+                              classs = c(rep("virginica", 40), rep("versicolor", 60), rep("setosa", 50)))
+
+# > d ---------------------------------------------------------------------
+library(class)
+
+train <- rbind(artificial_data[1:25,1:4,1], artificial_data[1:25,1:4,2], artificial_data[1:25,1:4,3])
+test <- rbind(artificial_data[26:50,1:4,1], artificial_data[26:50,1:4,2], artificial_data[26:50,1:4,3])
+cl <- factor(c(rep("s",25), rep("c",25), rep("v",25)))
+
+classifier_1 <- knn(train, test, cl, k = 3, prob=TRUE)
+
+# > e ---------------------------------------------------------------------
+
+train <- rbind(iris3[1:25,,1], iris3[1:25,,2], iris3[1:25,,3])
+test <- rbind(iris3[26:50,,1], iris3[26:50,,2], iris3[26:50,,3])
+cl <- factor(c(rep("s",25), rep("c",25), rep("v",25)))
+
+classifier_2 <- knn(train, test, cl, k = 3, prob=TRUE)
+
+
+# > f ---------------------------------------------------------------------
+
+# When we generated the data set artificial_data it was generated based on the
+# mean and standard deviation of the whole of the iris dataset, therefore a knn-classifier
+# can't distinguish the classes from eachother as there is no real difference between them.
+# When looking at classifier_1 each predicition is made with a probability of 0.33
+# as there are a third of a chance that it's either of the classes.
+# When looking at classifer_2 each prediction is made with changing probabilities,
+# as some points are easier to classify than others. 
