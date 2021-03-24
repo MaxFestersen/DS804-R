7545e3d19a5b60cd9dd3bfbefba496870e1bbdcd
ADernild <alex@dernild.dk>
2021-03-24 12:42:10 +0100

    Rettet 7.3

--- ex7/Another_try.R
+++ ex7/Another_try.R
@@ -9,4 +9,29 @@ dice_roller <- function(n) {
 lapply(lapply(c(5, 10, 50, 100, 1000, 10000), dice_roller), hist) # visualizing different n's 
 
 
+# Andet forsøg ----
+
+# Jeg troede at det antallet af kast, men det var antallet af terninger der er n.
+# Så jeg har lige skrevet noget nyt kode, der kan simulerer forskellige antal terninger istedet.
+
+dice_n <- list(1:6, 1:6, 1:6, 1:6, 1:6, 1:6, 1:6, 1:6, 1:6, 1:6) # list of 10 dice
+
+dices <- vector(mode = "list", length = length(dice_n)) # creating empty list
+
+for (i in 1:length(dice_n)) {
+  dice <- expand.grid(dice_n[1:i])
+  dice$total <- rowSums(dice)
+  dices[i] <- dice['total']
+} # rolling different amount of dices and finding rowSums
+
+dice_counts <- lapply(lapply(dices, as.data.frame), count, `X[[i]]`) # counting frequency of outcomes
+
+names(dice_counts) <- c("1 die", "2 dice", "3 dice", "4 dice", "5 dice", "6 dice", "7 dice", "8 dice", "9 dice", "10 dice") # naming counts
+
+i = 0
+lapply(dice_counts, function(x) {
+  i <<- i + 1
+  plot(x, main = names(dice_counts)[i])}) # plotting counts
+
+
 
