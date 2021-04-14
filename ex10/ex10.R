8ee633a03967b7a3097172b45e9a4ef6ec806925
ADernild <alex@dernild.dk>
2021-04-14 10:00:07 +0200

    Optics and all the other stuffs

--- ex10/ex10.R
+++ ex10/ex10.R
@@ -1,5 +1,8 @@
 library(Rlof)
 library(ROCR)
+library(readr)
+library(tidyverse)
+
 
 df <- data.frame(lab = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"),
                  x = c(1, 2, 1, 2, 3, 3, 3, 4, 4, 5, 7, 10, 10, 9, 10, 11, 9, 10, 11, 10),
@@ -101,3 +104,35 @@ plot(perf, main = "ROC-curve", sub = paste("k=4","AUC", auc), colorize = TRUE)
 
 X3clusters_and_noise_2d <- read_table2("3clusters-and-noise-2d.csv", 
                                        col_names = FALSE, comment = "#")
+
+cool_lof <- lof(X3clusters_and_noise_2d[1:2], 3, cores = 4)
+
+X3clusters_and_noise_2d$lof_3k <- cool_lof
+
+distance <- as.matrix(dist(X3clusters_and_noise_2d[1:2], "euclidean", T, T))
+
+Two_NN_dist <- c()
+Four_NN_dist <- c()
+sum_2NN <- c()
+sum_4NN <- c()
+
+for (i in 1:nrow(distance)) {
+  Two_NN_dist <- c(Two_NN_dist, nth(sort(distance[,i]), 3))
+  Four_NN_dist <- c(Four_NN_dist, nth(sort(distance[,i]), 5))
+  sum_2NN <- c(sum_2NN, sum(sort(distance[,i])[2:3]))
+  sum_4NN <- c(sum_4NN, sum(sort(distance[,i])[2:5]))
+}
+
+X3clusters_and_noise_2d$two_NN <- Two_NN_dist
+X3clusters_and_noise_2d$four_NN <- Four_NN_dist
+X3clusters_and_noise_2d$sum_2NN <- sum_2NN
+X3clusters_and_noise_2d$sum_4NN <- sum_4NN
+
+library(dbscan)
+
+optic_cluster <- optics(X3clusters_and_noise_2d[1:2], minPts = 5)
+
+plot(optic_cluster)
+
+
+plot(X3clusters_and_noise_2d$X1, X3clusters_and_noise_2d$X2)
