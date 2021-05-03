9a82aeadab7433f6ee3175ac415e80a41a208716
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-05-03 16:36:56 +0200

    Improved structure.

--- ex12/ex12.R
+++ ex12/ex12.R
@@ -2,14 +2,32 @@
 
 
 # a) ----------------------------------------------------------------------
+# Some dataset
+# TODO
+
+
+# > Decision trees --------------------------------------------------------
+
+
+
+
+# > The naïve Bayes classifier --------------------------------------------
+
+
+
+
+# > k nearest neighbor ----------------------------------------------------
+
 
 
 # b) ----------------------------------------------------------------------
-
+# How does the behavior of the k nearest neighbor classifier
+# change with the choice of k?
 
 # c) ----------------------------------------------------------------------
-
+# What is the impact of parameter choices on the quality of decision trees?
 
 # d) ----------------------------------------------------------------------
-
+# How does the behavior of the three classifiers change with the amount
+# of training data (e.g., choice of training-test-splits)?
 
