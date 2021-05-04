55a516cd5ff36f7806f17058dfd6122317f3458c
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-05-04 13:10:27 +0200

    Added training example for decision trees.

--- ex12/ex12.R
+++ ex12/ex12.R
@@ -77,6 +77,53 @@ orr.dose <- rpart(formula = dose ~ .,
       data = ToothGrowth
       )
 
+# Training data
+# Preparation:
+# > have data
+# > Set seed
+
+# Step 1: shuffle sorted data
+shuffle_index <- sample(1:nrow(ToothGrowth))
+head(shuffle_index)
+ToothGrowth <- ToothGrowth[shuffle_index, ]
+
+# Step 2: clean data
+# > Drop variables
+# >> eg. select or subset
+# > Create factor variables
+# >> eg. factor
+# > Drop the NA
+# >> Eg. na.omit
+
+# Note: not needed for ToothGrowth
+
+# Step 3: Training and test set creation
+# splitsting data into train and test data
+# > Base method
+splits <- sample(seq_len(nrow(ToothGrowth)), nrow(ToothGrowth)*0.8)
+train_tooth <- ToothGrowth[splits, ]
+test_tooth <- ToothGrowth[-splits, ]
+
+# > caTools method
+# splits <- sample.split(ToothGrowth, splitsRatio = 0.8) # 80/20 is most common, but do whatever
+# train_tooth <- subset(ToothGrowth, splits == "TRUE")
+# test_tooth <- subset(ToothGrowth, splits == "FALSE")
+
+# Optional: check dimentions
+dim(train_tooth)
+nrow(train_tooth)
+ncol(train_tooth)
+
+# Optional: use prep.table/table to vertify randomization
+prop.table(table(train_tooth$dose))
+
+# Step 4: Build model
+trained_tooth <- rpart(dose~., data = train_tooth, method = 'class')
+
+
+# Test training data
+
+
 # Visualize decision treee results
 rpart.plot(data.supp)
 rpart.plot(data.supp.method.class)
@@ -86,9 +133,9 @@ rpart.plot(data.dose.method.class)
 rpart.plot(data.dose.method.poisson)
 rpart.plot(orr.supp)
 rpart.plot(orr.dose)
+rpart.plot(trained_tooth)
 
 cat(paste(
-  "Note: results are not perfect, and some randomness is involved.",
   "Decision trees are not allways posible to create, which was the case for some of the tests.",
   "There is quite some randomness involved in the process. The result might the most optimal. One should try adjusting the formula to acchive reults.",
   "Working results can easily be identified by the labels apearing in the top right of the visualization.",
@@ -108,25 +155,25 @@ library(e1071)
 library(caTools)
 library(caret)
 
-# Splitting data into train and test data
-split <- sample.split(ToothGrowth, SplitRatio = 0.7)
-train_cl <- subset(ToothGrowth, split == "TRUE")
-test_cl <- subset(ToothGrowth, split == "FALSE")
+# splitsting data into train and test data - Done in previous section
+# splits <- sample.splits(ToothGrowth, splitsRatio = 0.7)
+# train_tooth <- subset(ToothGrowth, splits == "TRUE")
+# test_tooth <- subset(ToothGrowth, splits == "FALSE")
 
 # Feature Scaling
-train_scale <- scale(select(train_cl, -supp))
-test_scale <- scale(select(test_cl, -supp))
+train_scale <- scale(select(train_tooth, -supp))
+test_scale <- scale(select(test_tooth, -supp))
 
 # Fitting Naive Bayes Model 
 # to training dataset
-classifier_cl <- naiveBayes(supp ~ ., data = train_cl)
+classifier_cl <- naiveBayes(supp ~ ., data = train_tooth)
 classifier_cl
 
 # Predicting on test data'
-y_pred <- predict(classifier_cl, newdata = test_cl)
+y_pred <- predict(classifier_cl, newdata = test_tooth)
 
 # Confusion Matrix
-cm <- table(test_cl$supp, y_pred)
+cm <- table(test_tooth$supp, y_pred)
 cm
 
 # Model Evauation
