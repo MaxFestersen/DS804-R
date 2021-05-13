97aaae6a451fe668926a17bc13504007d709c23d
ADernild <alex@dernild.dk>
2021-05-13 11:26:39 +0200

    Playing around with neural net

--- ex14/ex14.R
+++ ex14/ex14.R
@@ -5,10 +5,13 @@
 
 # Dataset -----------------------------------------------------------------
 
-
+test <- read.table("datatest.txt", sep = ",")
+test2 <- read.table("datatest2.txt", sep = ",")
+training <- read.table("datatraining.txt", sep = ",")
 
 
 # Decision Tree -----------------------------------------------------------
+ 
 
 
 
@@ -19,5 +22,18 @@
 
 
 # Neural Network ----------------------------------------------------------
+library(neuralnet)
+
+training$Occupancy <- factor(training$Occupancy)
+
+net <- neuralnet(Occupancy ~ Temperature + Humidity + Light + CO2 + HumidityRatio,
+                 data = training,
+                 hidden = 3,
+                 linear.output = FALSE, 
+                 err.fct = 'ce', 
+                 likelihood = TRUE)
+
+
+plot(net)
 
 
