19d8f2af42bf381c7fed2704c991e7148f3d6f89
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-05-14 13:21:02 +0200

    Added test without light.

--- ex14/ex14.R
+++ ex14/ex14.R
@@ -64,7 +64,7 @@ predictions.c <- predict(tree.c, test, type = 'class') # predicting unseen test 
 cm.c <- table(test$Occupancy, predictions.c) # confusion matrix
 cluster_report(cm.c, cap = "Decision Tree with control") # Quality measures of Decision tree
 
-accuracy.test <- function(minsplit){
+accuracy.test <- function(minsplit, training, test){
   control <- rpart.control(minsplit = minsplit, minbucket = minsplit/2, cp = 0.001) # for adjusting hyperparameters
   tree <- rpart(Occupancy ~ .,
                 method = "class",
@@ -78,12 +78,61 @@ accuracy.test <- function(minsplit){
 # Control did not improve results. But maybe, we choose poorly. Let's try 128 different minsplit values.
 accuracy.arr <- c()
 for (i in 1:128) {
-  accuracy.arr <- c(accuracy.arr, accuracy.test(i))
+  accuracy.arr <- c(accuracy.arr, accuracy.test(i, training, test))
 }
 accuracy.arr
 max(accuracy.arr)
 
 # Control did not improve results, as the best split result was already reached
+
+# Light seems to be a very domminent attribute.
+# Date might also be an issue, as we will never test the results are based on a specific period
+# What if we remove it?
+# >> Formatting data ----
+remove.light <- function(x) {
+  x <- select(x, -Light) %>% 
+    select(-date)
+}
+
+test.f <- remove.light(test) # f as in filtered
+test2.f <- remove.light(test2)
+training.f <- remove.light(training)
+
+tree.f <- rpart(Occupancy ~ .,
+              method = "class",
+              data = training.f)
+rpart.plot(tree.f) # A bit complex?
+
+# predictions and repport
+predictions.f <- predict(tree.f, test.f, type = 'class') # predicting unseen test data
+cm.f <- table(test$Occupancy, predictions.f) # confusion matrix
+cluster_report(cm.f, cap = "Decision Tree without light") # Quality measures of Decision tree
+
+# Now with control parameters
+accuracy.f.arr <- c()
+for (i in 1:128) {
+  accuracy.f.arr <- c(accuracy.f.arr, accuracy.test(i, training.f, test.f))
+}
+accuracy.f.arr
+max(accuracy.f.arr)
+# huh. minsplit of 92-121 gives a better prediction.
+
+# Without light and with control with minsplit = 92
+control.c.f <- rpart.control(minsplit = 92, minbucket = 92/2, cp = 0.001) # for adjusting hyperparameters
+#tree <- rpart(Occupancy ~ Temperature + Humidity + Light + CO2 + HumidityRatio + date,
+tree.c.f <- rpart(Occupancy ~ .,
+                method = "class",
+                data = training.f,
+                control = control.c.f)
+rpart.plot(tree.c.f) # A bit better. Acctually the same now...
+
+
+# predictions and repport
+predictions.c.f <- predict(tree.c.f, test.f, type = 'class') # predicting unseen test data
+cm.c.f <- table(test$Occupancy, predictions.c.f) # confusion matrix
+cluster_report(cm.c.f, cap = "Decision Tree without light and minsplit = 92") # Quality measures of Decision tree
+
+
 
 ## Support Vectors and Margin (SVM)----------------------------------------
 svmfit <- svm(Occupancy ~ .,
