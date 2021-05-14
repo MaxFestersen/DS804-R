faa25ab570e6a5fbfc5cd4cbce25e1c29438d004
ADernild <alex@dernild.dk>
2021-05-14 13:46:02 +0200

    SVM rettet

--- ex14/ex14.R
+++ ex14/ex14.R
@@ -137,7 +137,7 @@ cluster_report(cm.c.f, cap = "Decision Tree without light and minsplit = 92") # 
 
 
 ## Support Vectors and Margin (SVM)----------------------------------------
-svmfit <- svm(Occupancy ~ .,
+svmfit <- svm(Occupancy ~ Temperature + Humidity + Light + CO2 + HumidityRatio + time,
               data = training,
               type = "C-classification",
               kernel = "radial",
@@ -146,7 +146,7 @@ svmfit <- svm(Occupancy ~ .,
               scale = TRUE)
 summary(svmfit)
 plot(svmfit, training, CO2 ~ HumidityRatio,
-     slice=list(Humidity=3, Light=4, date=5, Temperature = 6))
+     slice=list(Humidity=3, Light=4, time=5, Temperature = 6))
 predictions <- predict(svmfit, test, type = 'class') # predicting unseen test data
 cm <- table(test$Occupancy, predictions) # confusion matrix
 cluster_report(cm, cap = "Support-Vector-Machine") # Quality measures of SVM
